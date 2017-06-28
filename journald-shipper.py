fd5da27a779d3fedfed6f8a55ccd1dce134c3aae
David <david@likebook.me>
2017-06-28 14:11:38 +0200

    Change method into class

--- journald-shipper.py
+++ journald-shipper.py
@@ -16,179 +16,183 @@ import pytz
 import re
 from elasticsearch import Elasticsearch
 
-waitTime = 200  # In MS, to not add overload
-localTimezone = 'CET'
+
+class JournaldShipper:
+
+    waitTime = 200  # In MS, to not add overload
+    localTimezone = 'CET'
 
 
-def keycleaner(key):
-    """
-    Clean key to fit ES standards
-    :param key: str
-    :return: str
-    """
-    optimizedkey = key
-    if key[0] == '_':
-        optimizedkey = key[1:]
-    return optimizedkey.lower()
+    def __init__():
+        """
+        Main loop.
+        Used to seek events from Journald
+        """
+        # Create a systemd.journal.Reader instance
+        j = journal.Reader()
+
+        # Set the reader's default log level
+        j.log_level(journal.LOG_DEBUG)
+
+        # Only include entries since the current box has booted.
+        j.this_boot()
+        j.this_machine()
+
+        # Move to the end of the journal
+        j.seek_tail()
+
+        # Important! - Discard old journal entries
+        j.get_previous()
+
+        # Create a poll object for journal entries
+        p = select.poll()
+
+        # Register the journal's file descriptor with the polling object.
+        journal_fd = j.fileno()
+        poll_event_mask = j.get_events()
+        p.register(journal_fd, poll_event_mask)
+
+        # Poll for new journal entries at regular interval
+        while True:
+            if p.poll(waitTime) is not None:
+                if j.process() == journal.APPEND:
+                    for entry in j:
+                        insert_int_oes(prepare_es_payload(entry))
 
 
-def checkkeyallowance(key):
-    """
-    Check if key is allowed for data
-    :param key: dict
-    :return: bool
-    """
-    skippedkeys = {'_TRANSPORT', '_SOURCE_MONOTONIC_TIMESTAMP', '_SOURCE_REALTIME_TIMESTAMP', '_BOOT_ID', '_MACHINE_ID'}
-    if key in skippedkeys:
-        return False
-    if key[0:2] == '__':
-        return False
-    return True
+    def key_cleaner(key):
+        """
+        Clean key to fit ES standards
+        :param key: str
+        :return: str
+        """
+        optimizedkey = key
+        if key[0] == '_':
+            optimizedkey = key[1:]
+        return optimizedkey.lower()
 
 
-def prepareespayload(data):
-    """
-    Prepare payload for ES
-    :param data: dict
-    :return: dict
-    """
-    payload = dict()
-    for key in data:
-        if not checkkeyallowance(key):
-            # Special check for timestamp
-            if key == '__REALTIME_TIMESTAMP':
-                timestamp = data.get(key)
-                if not isinstance(timestamp, datetime.datetime):
-                    timestamp = datetime.datetime.now()
-                payload['@timestamp'] = timestamp
-            continue
-        payload[keycleaner(key)] = data.get(key)
-    if payload.get('@timestamp') is None:
-        payload['@timestamp'] = datetime.datetime.now()
-
-    # Current timezone is local
-    payload['@timestamp'] = payload['@timestamp'].astimezone(pytz.timezone(localTimezone))
-    # Correct payload is UTC
-    payload['@timestamp'] = payload['@timestamp'].astimezone(pytz.utc)
-
-    return split_payload(payload)
+    def check_key_allowance(key):
+        """
+        Check if key is allowed for data
+        :param key: dict
+        :return: bool
+        """
+        skippedkeys = {'_TRANSPORT', '_SOURCE_MONOTONIC_TIMESTAMP', '_SOURCE_REALTIME_TIMESTAMP', '_BOOT_ID', '_MACHINE_ID'}
+        if key in skippedkeys:
+            return False
+        if key[0:2] == '__':
+            return False
+        return True
 
 
-def split_payload(data):
-    """
-    Split payload into sub-arguments
-    :param data: dict
-    :return: dict
-    """
-    if (data['message'] is None):
+    def prepare_es_payload(data):
+        """
+        Prepare payload for ES
+        :param data: dict
+        :return: dict
+        """
+        payload = dict()
+        for key in data:
+            if not check_key_allowance(key):
+                # Special check for timestamp
+                if key == '__REALTIME_TIMESTAMP':
+                    timestamp = data.get(key)
+                    if not isinstance(timestamp, datetime.datetime):
+                        timestamp = datetime.datetime.now()
+                    payload['@timestamp'] = timestamp
+                continue
+            payload[keycleaner(key)] = data.get(key)
+        if payload.get('@timestamp') is None:
+            payload['@timestamp'] = datetime.datetime.now()
+
+        # Current timezone is local
+        payload['@timestamp'] = payload['@timestamp'].astimezone(pytz.timezone(localTimezone))
+        # Correct payload is UTC
+        payload['@timestamp'] = payload['@timestamp'].astimezone(pytz.utc)
+
+        return split_payload(payload)
+
+
+    def split_payload(data):
+        """
+        Split payload into sub-arguments
+        :param data: dict
+        :return: dict
+        """
+        if (data['message'] is None):
+            return data
+
+        sudo = r"\((?P<user>\S+)\)\sCMD\s\((?P<command>.*)\)"
+        sudo_matches = re.finditer(sudo, data['message'])
+        for match in sudo_matches:
+            data['user'] = match.group('user')
+            data['command'] = match.group('command')
+            data['type'] = 'sudo'
+
         return data
 
-    sudo = r"\((?P<user>\S+)\)\sCMD\s\((?P<command>.*)\)"
-    sudo_matches = re.finditer(sudo, data['message'])
-    for match in sudo_matches:
-        data['user'] = match.group('user')
-        data['command'] = match.group('command')
-        data['type'] = 'sudo'
 
-    return data
-
-
-def insertintoes(data):
-    """
-    Insert data into ElasicSearch
-    :param data: dict
-    :return:
-    """
-    timestamp = data.get('@timestamp')
-    logstashIndex = 'logstash-' + timestamp.strftime("%Y.%m.%d")
-    es = Elasticsearch()
-    if not es.indices.exists(logstashIndex):
-        # Setting mappings for index
-        mapping = '''
-            {
-                "mappings": {
-                      "_default_": {
-                        "_all": {
-                          "enabled": true,
-                          "norms": false
-                        },
-                        "dynamic_templates": [
-                          {
-                            "message_field": {
-                              "path_match": "message",
-                              "match_mapping_type": "string",
-                              "mapping": {
-                                "norms": false,
-                                "type": "text"
-                              }
-                            }
-                          },
-                          {
-                            "string_fields": {
-                              "match": "*",
-                              "match_mapping_type": "string",
-                              "mapping": {
-                                "fields": {
-                                  "keyword": {
-                                    "type": "keyword"
+    def insert_into_es(data):
+        """
+        Insert data into ElasicSearch
+        :param data: dict
+        :return:
+        """
+        timestamp = data.get('@timestamp')
+        logstashIndex = 'logstash-' + timestamp.strftime("%Y.%m.%d")
+        es = Elasticsearch()
+        if not es.indices.exists(logstashIndex):
+            # Setting mappings for index
+            mapping = '''
+                {
+                    "mappings": {
+                          "_default_": {
+                            "_all": {
+                              "enabled": true,
+                              "norms": false
+                            },
+                            "dynamic_templates": [
+                              {
+                                "message_field": {
+                                  "path_match": "message",
+                                  "match_mapping_type": "string",
+                                  "mapping": {
+                                    "norms": false,
+                                    "type": "text"
                                   }
-                                },
-                                "norms": false,
-                                "type": "text"
+                                }
+                              },
+                              {
+                                "string_fields": {
+                                  "match": "*",
+                                  "match_mapping_type": "string",
+                                  "mapping": {
+                                    "fields": {
+                                      "keyword": {
+                                        "type": "keyword"
+                                      }
+                                    },
+                                    "norms": false,
+                                    "type": "text"
+                                  }
+                                }
+                              }
+                            ],
+                            "properties": {
+                              "@timestamp": {
+                                "type": "date",
+                                "include_in_all": true
+                              },
+                              "@version": {
+                                "type": "keyword",
+                                "include_in_all": true
                               }
                             }
                           }
-                        ],
-                        "properties": {
-                          "@timestamp": {
-                            "type": "date",
-                            "include_in_all": true
-                          },
-                          "@version": {
-                            "type": "keyword",
-                            "include_in_all": true
-                          }
-                        }
-                      }
+                    }
                 }
-            }
-        '''
-        es.indices.create(logstashIndex, ignore=400, body=mapping)
+            '''
+            es.indices.create(logstashIndex, ignore=400, body=mapping)
 
-    es.index(index=logstashIndex, doc_type='systemd', timestamp=timestamp, body=data)
-
-
-"""
-Main loop.
-Used to seek events from Journald
-"""
-# Create a systemd.journal.Reader instance
-j = journal.Reader()
-
-# Set the reader's default log level
-j.log_level(journal.LOG_DEBUG)
-
-# Only include entries since the current box has booted.
-j.this_boot()
-j.this_machine()
-
-# Move to the end of the journal
-j.seek_tail()
-
-# Important! - Discard old journal entries
-j.get_previous()
-
-# Create a poll object for journal entries
-p = select.poll()
-
-# Register the journal's file descriptor with the polling object.
-journal_fd = j.fileno()
-poll_event_mask = j.get_events()
-p.register(journal_fd, poll_event_mask)
-
-# Poll for new journal entries at regular interval
-while True:
-    if p.poll(waitTime) is not None:
-        if j.process() == journal.APPEND:
-            for entry in j:
-                insertintoes(prepareespayload(entry))
+        es.index(index=logstashIndex, doc_type='systemd', timestamp=timestamp, body=data)
